e9c965593fd38f3c4d00e787f73f23704169a365
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-05-09 12:05:45 +0500

    Add players

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ let markO = gameBoard.markBoard("O");
 const displayController= (() => {
 
 })();
+
+function players(playerName, playerMark) {
+    const name = playerName;
+    const mark = playerMark;
+    let score = 0;
+    let turn = false;
+
+    const getName = () => name;
+    const getMark = () => mark;
+    const getScore = () => score;
+    const addScore = () => ++score;
+    const getTurn = () => turn;
+    const switchTurn = () => {(turn) ? turn=false:turn=true;}
+
+    return {getName, getMark, getScore, getTurn, switchTurn};
+};
+let player1 = players("Player1", "X");
+let player2 = players("Player2", "O");
